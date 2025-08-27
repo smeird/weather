1497a188a27d1b4a8f1694347ac49212623471c0
smeird <dom@smeird.com>
2025-08-27 14:46:40 +0100

    Use environment variables for database connection

--- dbconn.php
+++ dbconn.php
@@ -1,7 +1,8 @@
 <?php
 global $link;
- $link = mysqli_connect('localhost', 'webuser', 'WebUserPass0!','weewx');
-
-
-
+ $db_host = getenv('DB_HOST') ?: 'localhost';
+ $db_user = getenv('DB_USER') ?: 'webuser';
+ $db_pass = getenv('DB_PASSWORD') ?: 'WebUserPass0!';
+ $db_name = getenv('DB_NAME') ?: 'weewx';
+ $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 ?>
