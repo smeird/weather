458edf08309efe8990187bc1317511dc0be16d75
smeird <dom@smeird.com>
2025-08-28 11:38:46 +0100

    Add average row to annual temperature report

--- frontend/reporttempyeartotals.php
+++ frontend/reporttempyeartotals.php
@@ -33,6 +33,13 @@ $max_temps_per_month = array();
 $min_temps_per_month = array();
 $years_with_max_temp = array();
 $years_with_min_temp = array();
+// Initialize arrays for yearly averages
+$yearly_avg_sum = array();
+$yearly_avg_count = array();
+$yearly_max_sum = array();
+$yearly_max_count = array();
+$yearly_min_sum = array();
+$yearly_min_count = array();
 
 // Process the query results
 while ($row = mysqli_fetch_assoc($result)) {
@@ -41,6 +48,24 @@ while ($row = mysqli_fetch_assoc($result)) {
   $avg_temp = $row['avg_temp'];
   $max_temp = $row['max_temp'];
   $min_temp = $row['min_temp'];
+
+  // Initialize yearly arrays
+  if (!isset($yearly_avg_sum[$year])) {
+    $yearly_avg_sum[$year] = 0;
+    $yearly_avg_count[$year] = 0;
+    $yearly_max_sum[$year] = 0;
+    $yearly_max_count[$year] = 0;
+    $yearly_min_sum[$year] = 0;
+    $yearly_min_count[$year] = 0;
+  }
+
+  // Accumulate yearly sums and counts
+  $yearly_avg_sum[$year] += $avg_temp;
+  $yearly_avg_count[$year] += 1;
+  $yearly_max_sum[$year] += $max_temp;
+  $yearly_max_count[$year] += 1;
+  $yearly_min_sum[$year] += $min_temp;
+  $yearly_min_count[$year] += 1;
 
   // Store unique years
   if (!in_array($year, $years)) {
@@ -136,6 +161,25 @@ foreach ($months as $month) {
 
   echo "          </tr>";
 }
+
+// Add row for yearly averages
+echo "          <tr class=\"font-semibold bg-gray-100\">\n";
+echo "            <td class=\"px-4 py-2 text-left\">Average</td>\n";
+foreach ($years as $year) {
+  if (isset($yearly_avg_sum[$year]) && $yearly_avg_count[$year] > 0) {
+    $avg_avg = round($yearly_avg_sum[$year] / $yearly_avg_count[$year], 1);
+    $avg_max = round($yearly_max_sum[$year] / $yearly_max_count[$year], 1);
+    $avg_min = round($yearly_min_sum[$year] / $yearly_min_count[$year], 1);
+    echo "            <td class=\"px-4 py-2 text-right\">$avg_avg</td>\n";
+    echo "            <td class=\"px-4 py-2 text-right\">$avg_max</td>\n";
+    echo "            <td class=\"px-4 py-2 text-right\">$avg_min</td>\n";
+  } else {
+    echo "            <td class=\"px-4 py-2 text-right\">-</td>\n";
+    echo "            <td class=\"px-4 py-2 text-right\">-</td>\n";
+    echo "            <td class=\"px-4 py-2 text-right\">-</td>\n";
+  }
+}
+echo "          </tr>\n";
 
 echo "          </tbody>\n";
 echo "        </table>\n";
