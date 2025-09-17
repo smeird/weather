e607d4fc2386dd8cd1f2e17877a75891a6a0d0f4
smeird <dom@smeird.com>
2025-09-17 08:56:22 +0100

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -4,9 +4,9 @@
 global $link;
 
 $db_host = getenv('DB_HOST') ?: 'localhost';
-$db_user = getenv('DB_USER') ?: 'webuser';
-$db_pass = getenv('DB_PASSWORD') ?: 'WebUserPass0!';
-$db_name = getenv('DB_NAME') ?: 'weewx';
+$db_user = getenv('DB_USER') ?: 'user';
+$db_pass = getenv('DB_PASSWORD') ?: 'Pass0!';
+$db_name = getenv('DB_NAME') ?: 'db';
 
 // Create the connection and stop execution if it fails
 $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
